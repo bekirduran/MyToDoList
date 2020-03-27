381a9cf4613a53a65babe07e4b5b30daa037077a
Bekir Duran <akinmihal@gmail.com>
2020-03-27 16:23:35 +0300

    Initial Commit

---
+++ My Do To  List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Do To  List
+//
+//  Created by Bekir Duran on 27.03.2020.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
